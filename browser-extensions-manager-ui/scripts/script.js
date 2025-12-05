aed6f2e4e80f0138d9decbdbee8e5d5fd2dc28bf
Kauê Oliveira <kaueoliveira993@gmail.com>
2025-12-05 15:44:24 -0300

    feat: adicionar sistema de filtros e reestruturação da renderização das extensões

--- browser-extensions-manager-ui/scripts/script.js
+++ browser-extensions-manager-ui/scripts/script.js
@@ -1,84 +1,116 @@
 const sectionExtensions = document.getElementById("section-extensions");
 const endPoint = "data.json";
 
-const info = (endPoint, sectionExtensions) => {
-    fetch(endPoint)
-    .then((res) => res.json())
-    .then((res) => {
-        res.forEach((element) => {
-            const divExtension = document.createElement("div");
-            divExtension.className = "div-extension";
+let extensionsData = [];
+let filter = "All";
 
-            const divExtensionInfo = document.createElement("div");
-            divExtensionInfo.className = "extension-info";
+const renderExtensions = () => {
+  sectionExtensions.innerHTML = "";
 
-            const extensionImage = document.createElement("img");
-            extensionImage.src = element.logo;
-            extensionImage.alt = element.name;
+  extensionsData.forEach((element) => {
+    if (
+      filter === "All" ||
+      (filter === "Active" && element.active) ||
+      (filter === "Inactive" && !element.active)
+    ) {
+      const divExtension = document.createElement("div");
+      divExtension.className = "div-extension";
 
-            divExtensionInfo.appendChild(extensionImage);
+      const divExtensionInfo = document.createElement("div");
+      divExtensionInfo.className = "extension-info";
 
-            const divExtesionInfoText = document.createElement("div");
-            divExtesionInfoText.className = "extension-info-text";
+      const extensionImage = document.createElement("img");
+      extensionImage.src = element.logo;
+      extensionImage.alt = element.name;
 
-            const extensionName = document.createElement("h1");
-            extensionName.textContent = element.name;
+      divExtensionInfo.appendChild(extensionImage);
 
-            const extensionDescription = document.createElement("p");
-            extensionDescription.textContent = element.description;
+      const divExtesionInfoText = document.createElement("div");
+      divExtesionInfoText.className = "extension-info-text";
 
-            divExtesionInfoText.appendChild(extensionName);
-            divExtesionInfoText.appendChild(extensionDescription);
+      const extensionName = document.createElement("h1");
+      extensionName.textContent = element.name;
 
-            divExtensionInfo.appendChild(divExtesionInfoText);
+      const extensionDescription = document.createElement("p");
+      extensionDescription.textContent = element.description;
 
-            const divToggleRemove = document.createElement("div");
-            divToggleRemove.classList = "toggle-remove-wrapper";
+      divExtesionInfoText.appendChild(extensionName);
+      divExtesionInfoText.appendChild(extensionDescription);
 
-            const buttonRemove = document.createElement("button");
-            buttonRemove.className = "button-remove";
-            buttonRemove.textContent = "remove";
+      divExtensionInfo.appendChild(divExtesionInfoText);
 
-            const toggleSwitch = document.createElement("label");
-            toggleSwitch.className = "switch";
+      const divToggleRemove = document.createElement("div");
+      divToggleRemove.classList = "toggle-remove-wrapper";
 
-            const inputCheckBox = document.createElement("input");
-            inputCheckBox.className = "checkbox";
-            inputCheckBox.type = "checkbox";
+      const buttonRemove = document.createElement("button");
+      buttonRemove.className = "button-remove";
+      buttonRemove.textContent = "remove";
 
-            const slider = document.createElement("span");
-            slider.className = "slider";
+      // Function remove
 
-            divToggleRemove.appendChild(buttonRemove);
+      const toggleSwitch = document.createElement("label");
+      toggleSwitch.className = "switch";
 
-            toggleSwitch.appendChild(inputCheckBox);
-            toggleSwitch.appendChild(slider);
+      const inputCheckBox = document.createElement("input");
+      inputCheckBox.className = "checkbox";
+      inputCheckBox.type = "checkbox";
 
-            divToggleRemove.appendChild(toggleSwitch);
+      const slider = document.createElement("span");
+      slider.className = "slider";
 
-            divExtension.appendChild(divExtensionInfo);
-            divExtension.appendChild(divToggleRemove);
+      inputCheckBox.addEventListener("change", (e) => {
+        element.active = e.target.checked;
+        setTimeout(() => {
+            renderExtensions();
+        }, 500);
+      });
 
-            sectionExtensions.appendChild(divExtension);
-        });
-    });
+      divToggleRemove.appendChild(buttonRemove);
+
+      toggleSwitch.appendChild(inputCheckBox);
+      toggleSwitch.appendChild(slider);
+
+      divToggleRemove.appendChild(toggleSwitch);
+
+      divExtension.appendChild(divExtensionInfo);
+      divExtension.appendChild(divToggleRemove);
+
+      sectionExtensions.appendChild(divExtension);
+    }
+  });
 };
 
-info(endPoint, sectionExtensions);
+fetch(endPoint)
+  .then((res) => res.json())
+  .then((res) => {
+    extensionsData = res.map((ext) => ({ ...ext, active: false }));
+
+    renderExtensions()
+
+    listButtons.forEach((liButton) => {
+      liButton.addEventListener("click", (evt) => {
+        listButtons.forEach((btn) => btn.classList.remove("active"));
+        evt.target.classList.add("active");
+
+        filter = evt.target.textContent;
+        renderExtensions();
+      });
+    });
+  });
 
 const changeThemeBtn = document.getElementById("change-theme");
 const logoImage = document.getElementById("logo");
 
 function applyTheme(theme) {
-    if (theme === "light") {
-        document.body.classList.add("light");
-        logoImage.src = "assets/images/logo.svg";
-        changeThemeBtn.src = "assets/images/icon-moon.svg";
-    } else {
-        document.body.classList.remove("light");
-        logoImage.src = "assets/images/dark-logo.svg";
-        changeThemeBtn.src = "assets/images/icon-sun.svg"
-    }
+  if (theme === "light") {
+    document.body.classList.add("light");
+    logoImage.src = "assets/images/logo.svg";
+    changeThemeBtn.src = "assets/images/icon-moon.svg";
+  } else {
+    document.body.classList.remove("light");
+    logoImage.src = "assets/images/dark-logo.svg";
+    changeThemeBtn.src = "assets/images/icon-sun.svg";
+  }
 }
 
 // Carregar tema salvo no localStorage
@@ -86,7 +118,7 @@ const savedTheme = localStorage.getItem("theme") || "dark";
 applyTheme(savedTheme);
 
 changeThemeBtn.addEventListener("click", (evt) => {
-    const newTheme = document.body.classList.contains("light") ? "dark" : "light";
-    applyTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
-})
+  const newTheme = document.body.classList.contains("light") ? "dark" : "light";
+  applyTheme(newTheme);
+  localStorage.setItem("theme", newTheme);
+});
